2575f8e2ec51f62dc01736b121514f88b4a42a08
basamhassan01 <basamhassan01@gmail.com>
2024-12-11 16:11:14 +0800

    jquery fixed

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-$(document).ready(function() {
 
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
 
-    // cleck event on toggle menu
-    $toggleCollapse.click(function() {
-        $nav.toggleClass('collapse');
-    })
+jQuery(document).ready(function() {
 
-    // owl carousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
-    });
+  var $nav = jQuery('.nav');
+  var $toggleCollapse = jQuery('.toggle-collapse');
+
+  // Click event on toggle menu
+  $toggleCollapse.click(function() {
+      $nav.toggleClass('collapse');
+  });
+
+  // Owl carousel for blog
+  jQuery('.owl-carousel').owlCarousel({
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      dots: false,
+      nav: true,
+      navText: [jQuery('.owl-navigation .owl-nav-prev'), jQuery('.owl-navigation .owl-nav-next')]
+  });
 
 });
 
